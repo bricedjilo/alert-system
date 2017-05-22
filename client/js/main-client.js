db7ad20fe04bfef5c00a918f4098815c57a8d9f1
Brice Djilo <brice.djilo@hexcel.com>
2017-05-22 10:58:21 -0600

    adds font-awesome

---
+++ client/js/main-client.js
@@ -0,0 +1,32 @@
+
+Meteor.startup(function () {
+
+});
+
+Template.login.events({
+  'submit #login-form': function (e) {
+    e.preventDefault();
+    var form = $(e.target);
+    var username = form.find("#login-username").val();
+    var password = form.find("#login-password").val();
+
+    //if (username === "admin") {
+	// 	 Meteor.loginWithPassword(username, password, function (error) {
+	// 		 if ( ! error && password === "admin") {
+   //        // Set switch to trigger alert to change password
+   //        Session.setPersistent("passChangePrompt", 1);
+	// 	 	}
+	//  	});
+   //  } else {
+		 Meteor.loginWithLDAP(username, password,
+			{ dn: "uid=" + username + ",OU=Users,OU=SALT,OU=Sites,DC=hexcel,DC=com" },
+			function (error, success) {
+				if (error) {
+					console.log(error.reason);
+	       	} else {
+					console.log("logged in: " + username +" "+ password);
+				}
+			}
+	   );
+	}
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,16 @@
-// import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(function() {
-  // code to run on server at startup
+	LDAP_DEFAULTS.url = 'ldap://LONDNT201.Hexcel.com/OU=Users,OU=SALT,OU=Sites,DC=hexcel,DC=com';
+	LDAP_DEFAULTS.dn = 'OU=Users,OU=SALT,OU=Sites,DC=hexcel,DC=com';
+	LDAP_DEFAULTS.port = '';
+	LDAP_DEFAULTS.searchResultsProfileMap = [
+		{
+	      resultKey: 'cn',
+	      profileProperty: 'name'
+	    }
+	    ,{
+	      resultKey: 'mail',
+	      profileProperty: 'phoneNumber'
+	    }
+	 ]
 });
