0d3039be53c37bf8cb943d021f7923e8e3aa4b30
Brice Djilo <bd@bricedjilo.com>
2017-05-28 22:57:41 -0600

    makes login form transparent

--- client/js/main-config.js
+++ client/js/main-config.js
@@ -3,15 +3,39 @@ Router.configure({
     layoutTemplate: "ApplicationLayout"
 } );
 
-/*----------------- Routes -------------------------- */
 Router.route('/login', function() {
-   //  this.render("head", {to: "head"});
-    this.render("login", {to: "main"});
-   //  this.render("footer", {to: "footer"});
+    this.render("login");
 });
 
 Router.route('/', function() {
-   //  this.render("head", {to: "head"});
-    this.render("login", {to: "main"});
-   //  this.render("footer", {to: "footer"});
+    this.render("login");
+});
+
+Router.route('/dashboard', function() {
+    this.render("dashboard");
+});
+
+Router.onBeforeAction(function () {
+	if (!Meteor.userId()) {
+		this.render('login');
+  	} else {
+	  	this.next();
+  	}
+});
+
+/*----------------- css -------------------------------*/
+Template.login.onRendered(function() {
+  	$('body').addClass("hexcel");
+});
+
+
+/*----------------- Accounts -------------------------- */
+Accounts.onLogin(function() {
+	$('body').removeClass("hexcel");
+	Router.go('/dashboard');
+});
+
+Accounts.onLogout(function() {
+	$('body').addClass("hexcel");
+	Router.go('/login');
 });
